cfeb2cf7153514ee24ab452396f19ddff3613109
Igor Repka <igorrepka@LON-IREPKA-OSX.local>
2017-08-21 16:26:50 +0100

    Yaml configuration introduced, invocation of anypoint-cli from nodejs added, refactoring, yaml config file basic parsing logic added

--- anypoint_deployment_api.js
+++ anypoint_deployment_api.js
@@ -1,5 +1,40 @@
 console.log('--- Anypoint API is being invoked');
 
-anypoint-cli --username=$anypoint_username --password=$anypoint_password
+//load libraries
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+//get filename - passed as argument
+var filename = process.argv[2];
+if(filename) {
+	console.log('File contains all deployment config properties: ' + filename);
+} else {
+	console.error('ERROR: File argument is misssing!');	
+	process.exit(-1);
+}
+
+//parse config file
+try {
+    const config = yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
+    const indentedJson = JSON.stringify(config, null, 4);
+    console.log(indentedJson);
+} catch (e) {
+    console.log(e);
+    process.exit(-1);
+}
+
+
+//call anypoint cli API and parse results
+var exec = require('child_process').execSync;
+
+var result = exec('anypoint-cli --username=$anypoint_username --password=$anypoint_password --output json account user describe');
+
+console.log('Reuslt: ' + result);
+
+//convert to JSON
+var objResult = JSON.parse(result);
+
+console.log('Result obj email: ' + objResult.Email);
+console.log('Result obj user name: ' + objResult["User name"]);
 
 console.log('--- Anypoint API: all changes applied successfully');
--- deployment.sh
+++ deployment.sh
@@ -6,21 +6,15 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-#Parse deployment descriptor config file
-echo "=== Parsing deployment descriptor configuration file"
+#filename passed as script argument
 filename="$1"
-while read -r line
-do
-    data="$line"
-    echo "Data - $data"
-done < "$filename"
 
 #Dowload deployable package from repository
 echo "=== Downloading deployable package from repository"
 curl -O https://github.com/igor-repka/packages/raw/master/bid-processing-1.0.0-SNAPSHOT.zip
 
-echo '=== Anypoint API is being invoked'
-user="$(anypoint-cli --username=$anypoint_username --password=$anypoint_password --output json account user describe)"
-echo $user
+#Call Anypoint API to execute deployment and all the required configuration
+echo '=== Invoke Anypoint API'
+node anypoint_deployment_api.js $filename
 
 echo "=== Deployment has finished successfully! ==="

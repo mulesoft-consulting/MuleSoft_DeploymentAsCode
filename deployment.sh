d15b5f0740e4b77fb204dea1efe41453fbcd156e
Igor Repka <igorrepka@LON-IREPKA-OSX.local>
2017-08-21 11:26:58 +0100

    deployment script updated to read a config file

--- deployment.sh
+++ deployment.sh
@@ -1,1 +1,19 @@
-echo 'this is deployment script!'
+echo "Deployment has started!"
+
+#check input arguments
+if [[ $# -eq 0 ]] ; then
+    echo "ERROR: Please, pass the filename for deployment descritor as an argument!"
+    exit 1
+fi
+
+#Parse deployment descriptor config file
+filename="$1"
+while read -r line
+do
+    data="$line"
+    echo "Data - $data"
+done < "$filename"
+
+#Inoke Anypoint Platform API to deploy application
+
+echo "Deployment has finished successfuly!"
